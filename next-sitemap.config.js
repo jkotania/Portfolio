b9a6927eeb8744f8ffe1d6cc638c0d368a309751
jkotania <jkotania14@gmail.com>
2025-03-11 14:06:45 +0100

    Sitemap fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,6 +2,20 @@
 module.exports = {
   siteUrl: "https://jkotania.tech",
   generateRobotsTxt: true,
+  alternateRefs: [
+    {
+      href: 'https://jkotania.tech',
+      hreflang: 'x-default',
+    },
+    {
+      href: 'https://jkotania.tech',
+      hreflang: 'pl',
+    },
+    {
+      href: 'https://jkotania.tech',
+      hreflang: 'en',
+    },
+  ],
   robotsTxtOptions: {
     policies: [
       {
@@ -9,5 +23,19 @@ module.exports = {
         allow: "/",
       },
     ],
+  },
+  changefreq: 'monthly',
+  priority: 1.0,
+  transform: async (config, path) => {
+    // Upewnij się, że ścieżka zaczyna się od https://
+    const fullPath = path.startsWith('http') ? path : `${config.siteUrl}${path}`;
+    
+    return {
+      loc: fullPath,
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: new Date().toISOString(),
+      alternateRefs: config.alternateRefs ?? [],
+    }
   },
 };
